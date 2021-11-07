356681288f8eba9eba19c181370cf4eed38d0c5d
DiederiekVersluys <80745300+DiederiekVersluys@users.noreply.github.com>
2021-11-07 16:32:37 +0100

    Player and camera controls

--- core/src/com/obstacleavoid/screen/GameScreen.java
+++ core/src/com/obstacleavoid/screen/GameScreen.java
@@ -6,31 +6,83 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.obstacleavoid.config.GameConfig;
+import com.obstacleavoid.entity.Player;
 import com.obstacleavoid.util.GdxUtils;
 import com.obstacleavoid.util.ViewportUtils;
+import com.obstacleavoid.util.debug.DebugCameraController;
 
 public class GameScreen implements Screen {
+
 
     private OrthographicCamera camera;
     private Viewport viewport;
     private ShapeRenderer renderer;
+
+    private Player player;
+    private DebugCameraController debugCameraController;
 
     @Override
     public void show() {
         camera = new OrthographicCamera();
         viewport = new FitViewport(GameConfig.WORLD_WIDTH, GameConfig.WORLD_HEIGHT, camera);
         renderer = new ShapeRenderer();
+        player = new Player();
+
+
+//        float startPlayerX= GameConfig.WORLD_WIDTH /2f;
+//        float startPlayerY = 1;
+
+        float startPlayerX= 12;
+        float startPlayerY = 12;
+
+        player.setPosition(startPlayerX,startPlayerY);
+
+        //create CameraDebugController
+        debugCameraController = new DebugCameraController();
+        debugCameraController.setStartPosition(GameConfig.WORLD_CENTER_X, GameConfig.WORLD_CENTER_Y);
     }
 
     @Override
     public void render(float delta) {
+        debugCameraController.handleDebugInput(delta);
+        debugCameraController.applyTo(camera);
+
+        //update world
+        update(delta);
+
+        // clear screen
         GdxUtils.clearScreen();
+        //render debug graphics
+        renderDebug();
+
+
+    }
+
+    private void update(float delta) {
+        updatePlayer(delta);
+
+    }
+
+    private void updatePlayer(float delta) {
+
+        player.update();
+    }
+
+    private void renderDebug() {
+
+        renderer.setProjectionMatrix(camera.combined);
+        renderer.begin(ShapeRenderer.ShapeType.Line);
 
         drawDebug();
+        renderer.end();
+
+        ViewportUtils.drawGrid(viewport, renderer);
     }
 
     private void drawDebug() {
-        ViewportUtils.drawGrid(viewport, renderer);
+        player.drawDebug(renderer);
+
+
     }
 
     @Override
@@ -40,7 +92,7 @@ public class GameScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-        viewport.update(width, height);
+        viewport.update(width, height,true);
         ViewportUtils.debugPixelPerUnit(viewport);
     }
 
